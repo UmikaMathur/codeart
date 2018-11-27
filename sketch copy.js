f8570d26d262858c50d5d1c865c0113b47575116
Umika Mathur <umika.mathur@sjsu.edu>
2018-11-27 15:27:12 -0800

    update

--- sketch copy.js
+++ sketch copy.js
@@ -1,26 +1,27 @@
+var img;
+var img2;
+
+function preload() {
+img1 = loadImage("smiley_face.png");
+img2 = loadImage("illusion_image2.png");
+}
 function setup() {
-  createCanvas(500, 500);
-  background(200, 10, 100);
-  strokeWeight(3);
+createCanvas(windowWidth, windowHeight);
+
+}
+
+
+function mousePressed() {
+background(150, 10, 800);
+
 }
 
 function draw() {
-  if (mouseX > 400) {
-    //yellow
-    fill(255, 255, 0);
-  } else if (mouseX > 300) {
-    //violet
-    fill(101, 22, 232);
-  } else if (mouseX > 200) {
-    //green
-    fill(101, 250, 182);
-  } else if (mouseX > 100) {
-    //pink
-    fill(255, 142, 182);
-  } else {
-    //orange
-    fill(255, 142, 0);
-  }
-  	  triangle(50, 400, 150, 400, 100, 480);
-  quad(30, 100, 300, 450, 350, 150, 200, 50);
-  }
+  fill(100, 20, 400);
+  ellipse(mouseX + 100, mouseY + 100, 100, 100);
+
+}
+
+function changeCursor() {
+document.body.style.cursor = "wait";
+}
